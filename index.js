389994e0ee603ce05a7ee37c9fa7071157740f6d
idk707 <156857232+idk707@users.noreply.github.com>
2026-01-19 00:43:16 +0000

    Work In Progress

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 let currentIndex = 0;
+
+function prevSlide(){
+    const slides = document.getElementsByClassName("slide");
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[currentIndex].style.display = "block";
+}
+
+function nextSlide(){
+    const slides = document.getElementsByClassName("slide");
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[currentIndex].style.display = "block";
+}
